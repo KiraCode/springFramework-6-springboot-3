3025b96028c585be4e3c33685e43a179e2a5df98
KiraCode <tejasdj.5873@gmail.com>
2024-05-24 06:44:25 +0530

    adding project based on configuration properties

---
+++ SpringBoot-02/SpringBoot-Project/src/main/java/com/example/springboot/configproperties/ConfigPropertiesApplicationClass.java
@@ -0,0 +1,13 @@
+package com.example.springboot.configproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigPropertiesApplicationClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigPropertiesApplicationClass.class, args);
+	}
+
+}
---
+++ SpringBoot-02/SpringBoot-Project/src/main/java/com/example/springboot/learning/CourseApplicationMainClass.java
@@ -0,0 +1,13 @@
+package com.example.springboot.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplicationMainClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplicationMainClass.class, args);
+	}
+
+}
