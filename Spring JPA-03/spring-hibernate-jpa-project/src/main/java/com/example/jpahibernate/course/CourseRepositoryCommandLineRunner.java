688e677374fc552efb2137b6e873f18395756e6c
KiraCode <tejasdj.5873@gmail.com>
2024-05-27 12:34:53 +0530

    adding spring data jpa project

--- Spring JPA-03/spring-hibernate-jpa-project/src/main/java/com/example/jpahibernate/course/CourseRepositoryCommandLineRunner.java
+++ Spring JPA-03/spring-hibernate-jpa-project/src/main/java/com/example/jpahibernate/course/CourseRepositoryCommandLineRunner.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.example.jpahibernate.course.datajpa.CourseDataJpaRepository;
 import com.example.jpahibernate.course.jdbc.CourseJdbcRepositoy;
 import com.example.jpahibernate.course.jpa.CourseJpaRepository;
 
@@ -13,20 +14,24 @@ public class CourseRepositoryCommandLineRunner implements CommandLineRunner {
 //	@Autowired
 //	private CourseJdbcRepositoy repository;
 
+//	@Autowired
+//	private CourseJpaRepository repository;
+
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseDataJpaRepository repository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		repository.insert(new Course(1, "Devops JPA", "Scalar"));
-		repository.insert(new Course(2, "AWS JPA", "in28minutes"));
-		repository.insert(new Course(3, "Java JPA", "in28minutes"));
+		repository.save(new Course(1, "Devops JPA", "Scalar"));
+		repository.save(new Course(2, "AWS JPA", "in28minutes"));
+		repository.save(new Course(3, "Java JPA", "in28minutes"));
 
-		repository.delete(1);
+		repository.deleteById(1l);
 
-		System.out.println(repository.findById(2));
-		System.out.println(repository.findById(3));
+		System.out.println(repository.findById(2l));
+		System.out.println(repository.findById(3l));
+		System.out.println(repository.findByName("Java JPA"));
 
 	}
 
